33611342bd3b2460874fdbf3b532cf96d6597b5a
dgeop <dgeovanip@gmail.com>
2018-03-29 15:17:44 -0300

    Add files via upload

---
+++ Valores.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a, b;
+
+    printf("Digite o valor 1: ");
+    scanf("%d", &a);
+
+    printf("Digite o valor 2: ");
+    scanf("%d", &b);
+
+    printf("\nValor 1: %d\n", a);
+    printf("Valor 2: %d", b);
+
+    return 0;
+}
