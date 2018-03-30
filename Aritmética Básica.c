ba186b0eefcdad9bae53abd40594a006e60cec36
dgeop <dgeovanip@gmail.com>
2018-03-29 23:41:35 -0300

    Add files via upload

---
+++ Aritmética Básica.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main()
+{
+    int a, b, c;
+    a = 5;
+    b = 16;
+
+    c = a + b;
+    printf("Soma entre A + B = " "%d", c);
+
+    c = a - b;
+    printf("\nSubtracao entre A + B = " "%d", c);
+
+    c = a * b;
+    printf("\nMultiplicacao entre A + B = " "%d", c);
+
+    c = a / b;
+    printf("\nDivisao entre A + B = " "%d", c);
+}
